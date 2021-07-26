c5319afffca3ac8c8a80ddaee5a3575239acdee6
SiNam7 <muwon777@naver.com>
2021-07-26 23:57:07 +0900

    q10773 - 제로

---
+++ algorithm/Data Structure/q10773.java
@@ -0,0 +1,37 @@
+// https://www.acmicpc.net/problem/10773
+/*
+    Stack
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < N; i++) {
+
+            int cmd = Integer.parseInt(br.readLine());
+
+            if (cmd == 0) stack.pop();
+            else stack.push(cmd);
+
+        }
+
+        int ans = 0;
+        while (!stack.isEmpty()) {
+            ans += stack.pop();
+        }
+
+        System.out.println(ans);
+
+    }
+
+}
